ef8b3ccd271fb417413395c72d89fc3a86dd64d6
Furkan Kişi <furkankisi0@gmail.com>
2025-03-22 12:32:52 +0300

    sayfa yapısı oluşturuldu

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFeatherAlt } from "@fortawesome/free-solid-svg-icons";
+import { faHouse } from "@fortawesome/free-solid-svg-icons";
+import { faInbox } from "@fortawesome/free-solid-svg-icons";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+export default function App() {
+  return(
+    <div>
+      <div className='sidebar'>
+        <hr />
+        <div className='App-sidebar'>
+          <FontAwesomeIcon icon={faFeatherAlt} size="2x" className='icon'/>
+          <h1 className='Logo'>notes</h1>
+        </div>
+        <div className='sidebar-menu'>
+        <div className='sidebar-home'>
+          <FontAwesomeIcon icon={faHouse} size='1.5x' />
+            <h1>All Notes</h1>
+        </div>
+          <div className='sidebar-archived'>
+          <FontAwesomeIcon icon={faInbox} />
+          <h1>Archived Notes</h1>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
-
-export default App
